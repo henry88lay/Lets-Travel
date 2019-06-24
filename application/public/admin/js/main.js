fc0b18cdb19b3cf0d296007a81bb3e32cde367cd
Henry Lay <henry88lay@gmail.com>
2019-06-24 22:57:34 +1000

    added addPostBtn function towards the main.js

--- application/public/admin/js/main.js
+++ application/public/admin/js/main.js
@@ -17,3 +17,12 @@ document.addEventListener('DOMContentLoaded', async function() {
         articles.insertAdjacentHTML('beforeend', postHTML);
     })
 })
+
+addPostBtn.addEventListener('click', function () {
+    let articlesTab = document.getElementById('v-pills-articles');
+    articlesTab.classList.remove('show');
+    articlesTab.classList.remove('active');
+    let createTab = document.getElementById('v-pills-create-post');
+    createTab.classList.add('show');
+    createTab.classList.add('active');
+})
