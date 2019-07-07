f9d8dc7b128dbd0a604eb3188c2df03502da48d3
Henry Lay <henry88lay@gmail.com>
2019-07-07 22:47:40 +1000

    fixing the format for the codebase

--- application/app.js
+++ application/app.js
@@ -4,46 +4,48 @@ let mongoose = require('mongoose');
 let Post = require('./models/posts').Post;
 let multer = require('multer');
 
-mongoose.connect('mongodb://localhost/travels', { useNewUrlParser: true });
+mongoose.connect('mongodb://localhost/travels', {useNewUrlParser: true});
 app.use(express.json());
 let imageStorage = multer.diskStorage({
-    destination: (req, file, cb) => cb(null, 'public/images'),
-    filename: (req, file, cb) => cb(null, file.originalname)
-})
+  destination: (req, file, cb) => cb(null, 'public/images'),
+  filename: (req, file, cb) => cb(null, file.originalname)
+});
 
 app.use(multer({storage: ''}).single('imageFile'));
 
 let id = 1;
 
 app.get('/posts', async (req, resp) => {
-    let posts = await Post.find();
-    resp.send(posts);
-})
+  let posts = await Post.find();
+  resp.send(posts);
+});
 
 app.post('/posts', async (req, resp) => {
-    let reqBody = req.body;
+  let reqBody = req.body;
 
-    let imgPath;
-    if(reqBody.imageURL) {
-        imgPath = reqBody.imageURL;
-    } else {
-        imgPath = req.file.path.substring(req.file.path.indexOf('/'), req.file.path.length);
-    }
+  let imgPath;
+  if (reqBody.imageURL) {
+    imgPath = reqBody.imageURL;
+  } else {
+    imgPath = req.file.path.substring(
+      req.file.path.indexOf('/'),
+      req.file.path.length
+    );
+  }
 
-    let newPost = new Post ({
-        id: id++,
-        title: reqBody.title,
-        date: new Date(),
-        description: reqBody.description,
-        text: reqBody.text,
-        country: reqBody.country,
-        imageURL: imgPath
-    })
-    await newPost.save();
-    resp.send('Created');
-})
+  let newPost = new Post({
+    id: id++,
+    title: reqBody.title,
+    date: new Date(),
+    description: reqBody.description,
+    text: reqBody.text,
+    country: reqBody.country,
+    imageURL: imgPath
+  });
+  await newPost.save();
+  resp.send('Created');
+});
 
 app.use(express.static('public'));
 
 app.listen(3000, () => console.log('Listening 3000...'));
-
--- application/public/admin/js/main.js
+++ application/public/admin/js/main.js
@@ -1,11 +1,11 @@
-let addPostBtn = document.querySelector('.create-post-btn')
+let addPostBtn = document.querySelector('.create-post-btn');
 
 document.addEventListener('DOMContentLoaded', async function() {
-    let posts = await getPosts();
-    let articles = document.querySelector('.articles');
-    articles.innerHTML = '';
-    posts.forEach((post) => {
-        let postHTML = `
+  let posts = await getPosts();
+  let articles = document.querySelector('.articles');
+  articles.innerHTML = '';
+  posts.forEach(post => {
+    let postHTML = `
         <article class="d-flex justify-content-between align-items-center article-inline">
             <div class="id w5">${post.id}</div>
             <div class="name w30">${post.title}</div>
@@ -14,15 +14,15 @@ document.addEventListener('DOMContentLoaded', async function() {
             <div class="edit w10"><button class="btn btn-link">Edit</button></div>
             <div class="remove w5"><button class="btn btn-link">X</button></div>
         </article>`;
-        articles.insertAdjacentHTML('beforeend', postHTML);
-    })
-})
+    articles.insertAdjacentHTML('beforeend', postHTML);
+  });
+});
 
-addPostBtn.addEventListener('click', function () {
-    let articlesTab = document.getElementById('v-pills-articles');
-    articlesTab.classList.remove('show');
-    articlesTab.classList.remove('active');
-    let createTab = document.getElementById('v-pills-create-post');
-    createTab.classList.add('show');
-    createTab.classList.add('active');
-})
+addPostBtn.addEventListener('click', function() {
+  let articlesTab = document.getElementById('v-pills-articles');
+  articlesTab.classList.remove('show');
+  articlesTab.classList.remove('active');
+  let createTab = document.getElementById('v-pills-create-post');
+  createTab.classList.add('show');
+  createTab.classList.add('active');
+});
